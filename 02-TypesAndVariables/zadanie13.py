32bd2df117e87c3947bbf2e4cba6bb1ac7f7f511
Kamila Łopatowska <146941710+KamilaLopatowska@users.noreply.github.com>
2023-10-11 12:52:07 +0200

    Add files via upload

---
+++ 02-TypesAndVariables/zadanie13.py
@@ -0,0 +1,3 @@
+cube_side_length = int(input("Enter cube side: "))
+cube_surface_area = 6*cube_side_length*cube_side_length
+print(f'The surface area of a cube with side {cube_side_length} is {cube_surface_area}')
