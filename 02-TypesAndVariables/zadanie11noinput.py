fac430eba744c1cbf1171de654b0f702e6779c33
Kamila Łopatowska <146941710+KamilaLopatowska@users.noreply.github.com>
2023-10-11 12:45:36 +0200

    Add files via upload

---
+++ 02-TypesAndVariables/zadanie11noinput.py
@@ -0,0 +1,6 @@
+father_income = 1000
+mother_income = 1000
+family_members = 4
+total_income = father_income + mother_income
+income_per_person = total_income/family_members
+print(f"Enter father's income {father_income}\n Enter mother's income: {mother_income}\n Enter number of people in family: {family_members}\n Total income: {total_income}\n Income per person: {income_per_person}")
---
+++ 02-TypesAndVariables/zadanie12.py
@@ -0,0 +1,4 @@
+name = input("Enter your first name: ")
+surname = input("Enter your surname: ")
+full_name = name + ' ' + surname
+print(f'Your full name is {full_name}')
